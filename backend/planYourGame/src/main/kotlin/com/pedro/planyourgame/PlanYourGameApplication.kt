249dd44c07a896755a1a567ed3d29dafd19736b1
Pedro Vieira <pedrovieira1355@gmail.com>
2026-02-14 16:59:27 -0300

    build: add Spring Boot application configuration

---
+++ backend/planYourGame/src/main/kotlin/com/pedro/planyourgame/PlanYourGameApplication.kt
@@ -0,0 +1,11 @@
+package com.pedro.planYourGame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanYourGameApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanYourGameApplication>(*args)
+}
---
+++ backend/planYourGame/src/test/kotlin/com/pedro/planyourgame/PlanYourGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pedro.planYourGame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanYourGameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
