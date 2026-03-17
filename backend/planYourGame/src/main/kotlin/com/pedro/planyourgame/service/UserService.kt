19c4aa054a23933000fb7bba74b08f623bdc756a
Pedro Vieira <pedrovieira1355@gmail.com>
2026-03-17 19:51:09 -0300

    feat: add the UserDTO

--- backend/planYourGame/src/main/kotlin/com/pedro/planyourgame/service/UserService.kt
+++ backend/planYourGame/src/main/kotlin/com/pedro/planyourgame/service/UserService.kt
@@ -1,12 +1,17 @@
 package com.pedro.planyourgame.service
 
 import com.pedro.planyourgame.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class UserService(private val userRepository: UserRepository){
-    
+class UserService(
+    private val userRepository: UserRepository,
 
-
+): UserDetailsService {
+    override fun loadUserByUsername(login: String): UserDetails? {
+        return userRepository.findByLogin(login).orElseThrow{() -> }
+    }
 }
 
